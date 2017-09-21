d91a607f6653c65239a6bfa291c6eba3c14f0b1e
Charles <char-cou@hotmail.fr>
2017-09-21 15:45:34 -0400

    Creating TextminingModule.js, DAOs for symptoms and files for text mining

--- test/UpdateModuleTest.js
+++ test/UpdateModuleTest.js
@@ -1,3 +1,58 @@
-let UpdateModule = require("../local_node_modules/controller/UpdateModule.js");
+global.__base = __dirname + '/';
 
-UpdateModule.noUpdateRareDiseaseWorkflow();
+let UpdateModule = require("../local_node_modules/controller/UpdateModule.js");
+let NCBIAPIModule = require("../local_node_modules/controller/NCBIAPIModule.js");
+var async = require("async");
+
+//UpdateModule.noUpdateRareDiseaseWorkflow();
+
+NCBIAPIModule.callNCBIAPI(
+    NCBIAPIModule.getIdsFromSearch,
+    [
+        "Idiopathic achalasia",
+        930,
+        //Called when esearch finished and ids acquired
+        function(idlist, orphanetID)
+        {
+            if(idlist != undefined && orphanetID != undefined)
+            {
+                async.each(
+                    idlist,
+                    function(id, next)
+                    {
+                        NCBIAPIModule.callNCBIAPI(
+                            NCBIAPIModule.getPublication,
+                            [
+                                id,
+                                function(publication)
+                                {
+                                    UpdateModule.updateSymptoms(
+                                        publication,
+                                        orphanetID,
+                                        function()
+                                        {
+                                            next
+                                            //console.log("I'm a stupid student");
+                                        }
+                                    );
+                                }
+                            ]
+                        );
+                    },
+                    function(err)
+                    {
+                        if(err)
+                        {
+
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                );
+            }
+        }
+    ]
+);
+
